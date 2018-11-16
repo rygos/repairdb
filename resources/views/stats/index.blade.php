b5cc8d69b82213a8abf935f76930d33839043717
rygos <rygos@icloud.com>
2018-11-16 11:59:25 +0100

    Add Stats page

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -7,8 +7,32 @@
             <th>VALUE</th>
         </tr>
         <tr>
-            <td>Repairs Closed</td>
+            <td>Repairs</td>
+            <td>{{ \App\Models\Repair::count() }}</td>
+        </tr>
+        <tr>
+            <td>Repairs Open</td>
             <td>{{ \App\Models\Repair::whereNotIn('closing_reason_id', [2,11])->orWhereNull('closing_reason_id')->count() }}</td>
+        </tr>
+        <tr>
+            <td>Repairs Success</td>
+            <td>{{ \App\Models\Repair::whereClosingReasonId(2)->count() }}</td>
+        </tr>
+        <tr>
+            <td>Repairs NDF</td>
+            <td>{{ \App\Models\Repair::whereClosingReasonId(11)->count() }}</td>
+        </tr>
+        <tr>
+            <td>Parts in DB</td>
+            <td>{{ \App\Models\Spare::count() }}</td>
+        </tr>
+        <tr>
+            <td>Parts ordered</td>
+            <td>{{ \App\Models\SparesToRepair::count() }}</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
         </tr>
     </table>
 @endsection
