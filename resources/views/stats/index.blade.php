3db2c5a65b0ea7eb28feb0fd419d5493d08194c6
rygos <rygos@icloud.com>
2018-12-19 10:09:17 +0100

    add columns to stats page

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -6,7 +6,7 @@
             <th>NAME</th>
             <th>SUM</th>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <th>MH</th>
                     <th>SB</th>
                     <th>DB</th>
@@ -17,7 +17,7 @@
             <td>Repairs</td>
             <td>{{ \App\Models\Repair::count() }}</td>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <td>{{ \App\Models\Repair::whereUserId(1)->count() }}</td>
                     <td>{{ \App\Models\Repair::whereUserId(2)->count() }}</td>
                     <td>{{ \App\Models\Repair::whereUserId(3)->count() }}</td>
@@ -28,7 +28,7 @@
             <td>Repairs Open</td>
             <td>{{ \App\Models\Repair::whereNotIn('closing_reason_id', [2,11])->orWhereNull('closing_reason_id')->count() }}</td>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <td>{{\App\Models\Repair::where('user_id', '=', 1)->whereNotIn('closing_reason_id', [2,11])->orWhereNull('closing_reason_id')->count() }}</td>
                     <td>{{\App\Models\Repair::where('user_id', '=', 2)->whereNotIn('closing_reason_id', [2,11])->orWhereNull('closing_reason_id')->count() }}</td>
                     <td>{{\App\Models\Repair::where('user_id', '=', 3)->whereNotIn('closing_reason_id', [2,11])->orWhereNull('closing_reason_id')->count() }}</td>
@@ -39,7 +39,7 @@
             <td>Repairs Success</td>
             <td>{{ \App\Models\Repair::whereClosingReasonId(2)->count() }}</td>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <td>{{ \App\Models\Repair::whereClosingReasonId(2)->where('user_id', '=', 1)->count() }}</td>
                     <td>{{ \App\Models\Repair::whereClosingReasonId(2)->where('user_id', '=', 2)->count() }}</td>
                     <td>{{ \App\Models\Repair::whereClosingReasonId(2)->where('user_id', '=', 3)->count() }}</td>
@@ -50,7 +50,7 @@
             <td>Repairs NDF</td>
             <td>{{ \App\Models\Repair::whereClosingReasonId(11)->count() }}</td>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <td>{{ \App\Models\Repair::whereClosingReasonId(11)->where('user_id', '=', 1)->count() }}</td>
                     <td>{{ \App\Models\Repair::whereClosingReasonId(11)->where('user_id', '=', 2)->count() }}</td>
                     <td>{{ \App\Models\Repair::whereClosingReasonId(11)->where('user_id', '=', 3)->count() }}</td>
@@ -61,7 +61,7 @@
             <td>Parts in DB</td>
             <td>{{ \App\Models\Spare::count() }}</td>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <td>-</td>
                     <td>-</td>
                     <td>-</td>
@@ -72,7 +72,7 @@
             <td>Parts ordered</td>
             <td>{{ \App\Models\SparesToRepair::count() }}</td>
             @if(Auth::check())
-                @if(Auth::id() == 1)
+                @if(Auth::id() != 3)
                     <td>-</td>
                     <td>-</td>
                     <td>-</td>
