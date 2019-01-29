c183f16483645ef5f3105a67e72a003d4fcb666b
rygos <rygos@icloud.com>
2019-01-29 14:25:18 +0100

    search for gno and orderno, highlight search string

--- resources/views/_partials/repair_table.blade.php
+++ resources/views/_partials/repair_table.blade.php
@@ -87,7 +87,7 @@
 
 @if(isset($term))
     <script>
-        var src_str = $("tbody").html();
+        var src_str = $().html();
         var term = "{{ $term }}";
         term = term.replace(/(\s+)/,"(<[^>]+>)*$1(<[^>]+>)*");
         var pattern = new RegExp("("+term+")", "gi");
@@ -95,6 +95,6 @@
         src_str = src_str.replace(pattern, "<mark>$1</mark>");
         src_str = src_str.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/,"$1</mark>$2<mark>$4");
 
-        $("tbody").html(src_str);
+        $().html(src_str);
     </script>
 @endif
