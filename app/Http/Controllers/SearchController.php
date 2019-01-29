6399024a634644f09ed33f376664c8f7b9a684c8
rygos <rygos@icloud.com>
2019-01-29 14:14:49 +0100

    search for gno and orderno, highlight search string

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -34,13 +34,22 @@ class SearchController extends Controller
             $res_unit_ids[] = $i->id;
         }
 
+        $result = new \Illuminate\Database\Eloquent\Collection;
+
         $reprminst = Repair::whereIn('rminstzlb_id', $res_rminst_ids)->get();
         $repunits = Repair::whereIn('unit_id', $res_unit_ids)->get();
 
         $result = $reprminst->merge($repunits);
 
+        $reporder = Repair::where('order_no', 'LIKE', "%$term%")->get();
+        $result = $result->merge($reporder);
+
+        $repgno = Repair::where('g_no', 'LIKE', "%$term%")->get();
+        $result = $result->merge($repgno)->sortByDesc('id', 'ASC');
+
         return view('home.index', [
             'data' => $result,
+            'term' => $term,
         ]);
     }
 }
