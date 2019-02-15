57ce37583d4db1934ec0abadf137003690627e64
rygos <rygos@icloud.com>
2019-02-15 14:37:25 +0100

    get model from serial number is present. if not, add it

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 class ModelController extends Controller
 {
     public function checkeeee(Request $request){
-        $eeee = EeeeModel::whereEeee($request->post('eeee'))->first();
+        $eeee = EeeeModel::whereEeee($request->post('eeee'))->get();
 
-        if($eeee){
+        if($eeee->count() != 0){
             return "TRUE";
         }else{
             return "FALSE";
