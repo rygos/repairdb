8b37228c3abb8568cf349c6037c09372212c1de8
rygos <rygos@icloud.com>
2019-02-15 14:44:00 +0100

    fixed

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -8,9 +8,8 @@ use Illuminate\Http\Request;
 class ModelController extends Controller
 {
     public function checkeeee(Request $request){
-        $eeee = EeeeModel::whereEeee($request->post('eeee'))->get();
+        $eeee = EeeeModel::whereEeee(substr($request->post('eeee'), -4))->get();
 
-        dd($eeee);
         if($eeee->count() != 0){
             return "TRUE";
         }else{
