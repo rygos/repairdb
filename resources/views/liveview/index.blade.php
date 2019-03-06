5076a0e7486e4841a2ae5a0b4d5f816f5d5c0b13
rygos <rygos@icloud.com>
2019-03-06 11:56:02 +0100

    autorefresh liveview every 10 seconds

--- resources/views/liveview/index.blade.php
+++ resources/views/liveview/index.blade.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <meta http-equiv="refresh" content="10; URL=http://repair.rmarchiv.tk/liveview">
     <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
     <!-- latest -->
     <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet"/>
