49bdb5ec6f663c0ffeffebafed397644a529ec76
rygos <rygos@icloud.com>
2019-03-21 10:13:53 +0100

    OvertimeController sends data

--- app/Http/Controllers/OvertimeController.php
+++ app/Http/Controllers/OvertimeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Overtime;
 use Illuminate\Http\Request;
 
 class OvertimeController extends Controller
 {
     public function index(){
+        $data = Overtime::whereUserId(\Auth::id())->orderBy('created_at', 'asc')->get();
 
+        return view('overtime.index', [
+            'data' => $data,
+        ]);
     }
 }
