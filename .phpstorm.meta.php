badfe11a6269f5f526adb40303f486335dc237af
Anvil Palamattam <anvil008@me.com>
2020-01-24 14:22:28 +0100

    add customer

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-01-24 12:43:47.
+    * Generated on 2020-01-24 12:45:14.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
