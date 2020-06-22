44228d9626f3ffb6547de5890a3ff26fbaccedd2
rygos <rygos@icloud.com>
2020-06-22 19:35:39 +0200

    xcharge on status change

--- app/Http/Controllers/RepairController.php
+++ app/Http/Controllers/RepairController.php
@@ -295,9 +295,10 @@ Fremdverschulden: &#13;
         //build crosscharge positions
         //check for existing rows
         $xcharge_check = CrossCharge::whereRepairId($rep->id)->get()->count();
-        dd($xcharge_check);
+
         if($xcharge_check != 0){
             //check for status and ndf
+            dd($rep->closing_reason_id);
             if($rep->closing_reason_id == 2 or $rep->closing_reason_id == 11){
                 dd($rep->model->model_type_xcharge_id);
             }
