40db13a45cd8f78de94a3e4ef52e900282dba94f
rygos <rygos@icloud.com>
2021-10-22 16:16:50 +0200

    start with admin panel

--- resources/views/admin/show_user.blade.php
+++ resources/views/admin/show_user.blade.php
@@ -17,6 +17,37 @@
                             <td>Email:</td>
                             <td>{{ Form::text('email', $user->email) }}</td>
                         </tr>
+                        <tr>
+                            <td colspan="2">Permissions</td>
+                        </tr>
+                        <tr>
+                            <td>Überstunden Panel:</td>
+                            <td>{{ Form::checkbox('access_overtime', $user->access_overtime) }}</td>
+                        </tr>
+                        <tr>
+                            <td>XCharge:</td>
+                            <td>{{ Form::checkbox('access_xcharge', $user->access_xcharge) }}</td>
+                        </tr>
+                        <tr>
+                            <td>Verschrottung:</td>
+                            <td>{{ Form::checkbox('access_scrape', $user->access_scrape) }}</td>
+                        </tr>
+                        <tr>
+                            <td>Ersatzteile:</td>
+                            <td>{{ Form::checkbox('access_parts', $user->access_parts) }}</td>
+                        </tr>
+                        <tr>
+                            <td>Reports/Faktura:</td>
+                            <td>{{ Form::checkbox('access_reports', $user->access_reports) }}</td>
+                        </tr>
+                        <tr>
+                            <td>Repair:</td>
+                            <td>{{ Form::checkbox('access_repairs', $user->access_repairs) }}</td>
+                        </tr>
+                        <tr>
+                            <td>Admin:</td>
+                            <td>{{ Form::checkbox('access_admin', $user->access_admin) }}</td>
+                        </tr>
                         <tr><td></td><td>{{ Form::submit() }}</td></tr>
                     </table>
                 </div>
