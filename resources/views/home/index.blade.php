cf808ca0933130b805841e899427ea2c9446d575
rygos <rygos@icloud.com>
2021-11-04 10:44:26 +0100

    get repair location from assigned user

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,8 +1,10 @@
 @extends('_layout')
 @section('title', 'Home')
 @section('content')
-@include('_partials.repair_table', $data)
-    @isset($new)
+    @if(!$new)
+        @include('_partials.repair_table', $data)
+    @else
+        @include('_partials.repair_table', $data)
         @include('_partials.new_table', $new)
-    @endisset
+    @endif
 @endsection
